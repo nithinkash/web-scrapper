a209667edad0597954d753c8186425409e6d22f6
nithinkash <nitinkashyapa@gmail.com>
2019-01-06 11:13:59 +0530

    python scrapper

---
+++ scrapper.py
@@ -0,0 +1,23 @@
+import requests
+from bs4 import BeautifulSoup
+
+quote_page=requests.get('https://www.brainyquote.com/quote_of_the_day')
+html = quote_page.text
+
+soup=BeautifulSoup(html,'html.parser')
+
+#print(soup)
+
+name_box = soup.find('a', attrs={'class': 'b-qt qt_118824 oncl_q'})
+name = name_box.text.strip() # strip() is used to remove starting and trailing
+
+print ('//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////')
+print ('//	welcome to the daily quotes		 ')
+print ('//		todays Love quote 			 ')
+print ('//	{}{{}{}{}{}{}{}{{}{}{}{}{}{}{}{}{}{}{}	 ')
+print (name)				
+print ('//						 ')	
+print ('//						 ')	
+print ('/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////')
+
+
